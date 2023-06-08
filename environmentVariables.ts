dddc6ec8b5d78a099ce8835e970ee4f2163f6984
Pedro Xavier <xavierpedro14@gmail.com>
2023-06-08 19:11:33 +0100

    Minor fixes

--- environmentVariables.ts
+++
@@ -1,3 +0,0 @@
-const { API_HOST = "http://127.0.0.1:5005" } = process.env;
-
-export { API_HOST };
--- lib/axios.ts
+++ lib/axios.ts
@@ -1,6 +1,5 @@
 import axios from "axios";
-import { API_HOST } from "../environmentVariables";
 
 export const api = axios.create({
-  baseURL: API_HOST,
+  baseURL: process.env.API_HOST || "http://localhost:5005",
 });
